4323afaea3cf8c8904bed944e87c8a30728b5321
Spartaco17 <160352499+Spartaco17@users.noreply.github.com>
2024-02-19 18:07:22 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/Star/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/Star/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/Spartaco17/Star/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc']
+    let rpcEndpoints = ['https://rpc.hellomoon.io/4b9f787f-9492-474d-afba-b73c1045daaa','https://solana-mainnet.g.alchemy.com/v2/IcOD6tajZ-Gtj7OxuRNfWy-jQL8AJONr']
 
     const connectionProxy = {
         get(target, key, receiver) {
